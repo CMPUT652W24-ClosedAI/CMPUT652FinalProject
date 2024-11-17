f98a633dfba4b83a86b7c5bc558f43d8eadd6961
Seth Akins <sethlinc4@gmail.com>
2024-11-17 10:55:53 -0700

    Wrote function to get squared difference for the value functions of the initial states

---
+++ generator/get_fairness_score.py
@@ -0,0 +1,86 @@
+# http://proceedings.mlr.press/v97/han19a/han19a.pdf
+
+import random
+
+import numpy as np
+import torch
+from gym.spaces import MultiDiscrete
+from stable_baselines3.common.vec_env import VecMonitor
+
+from experiments.ppo_gridnet import MicroRTSStatsRecorder, Agent
+from gym_microrts import microrts_ai  # noqa
+from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv
+
+
+def squared_value_difference(
+    map_path: str = "maps/16x16/basesWorkers16x16A.xml",
+    seed: int = 0,
+    model_path: str = "gym-microrts-static-files/agent_sota.pt",
+) -> float:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+    ais = []
+    envs = MicroRTSGridModeVecEnv(
+        num_bot_envs=len(ais),
+        num_selfplay_envs=2,
+        partial_obs=False,
+        max_steps=1,
+        render_theme=2,
+        ai2s=ais,
+        map_paths=[map_path],
+        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0]),
+    )
+    assert isinstance(
+        envs.action_space, MultiDiscrete
+    ), "only MultiDiscrete action space is supported"
+
+    agent = Agent(envs).to(device)
+    agent2 = Agent(envs).to(device)
+
+    # Note how `next_obs` and `next_done` are used; their usage is equivalent to
+    # https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail/blob/84a7582477fb0d5c82ad6d850fe476829dddd2e1/a2c_ppo_acktr/storage.py#L60
+    next_obs = torch.Tensor(envs.reset()).to(device)
+
+    agent.load_state_dict(torch.load(model_path, map_location=device))
+    agent.eval()
+    agent2.load_state_dict(torch.load(model_path, map_location=device))
+    agent2.eval()
+
+    envs.render()
+    # ALGO LOGIC: put action logic here
+    with torch.no_grad():
+        invalid_action_masks = torch.tensor(np.array(envs.get_action_mask())).to(device)
+
+        p1_obs = next_obs[::2]
+        p2_obs = next_obs[1::2]
+        p1_mask = invalid_action_masks[::2]
+        p2_mask = invalid_action_masks[1::2]
+
+        p2_obs_reversed = torch.flip(p2_obs, dims=[1, 2])
+        p2_mask_reversed = torch.flip(p2_mask, dims=[1])
+
+        # 1, 256, 7
+        p1_action, _, _, _, p1_value = agent.get_action_and_value(
+            p1_obs, envs=envs, invalid_action_masks=p1_mask, device=device
+        )
+
+        p2_action, _, _, _, p2_value = agent2.get_action_and_value(
+            p2_obs_reversed,
+            envs=envs,
+            invalid_action_masks=p2_mask_reversed,
+            device=device,
+        )
+
+    # TODO: I am skipping this but likely causing a memory leak
+    # envs.close()
+
+    return (p1_value - p2_value) ** 2
+
+
+if __name__ == "__main__":
+    print(squared_value_difference())
+    print(squared_value_difference())
--- generator/map_converter.py
+++ generator/map_converter.py
@@ -8,22 +8,27 @@ from generator.enums import LayerName
 
 ALLOWED_RESOURCE_VALUES = {5, 10, 15, 20}
 
+
 def convert_xml(xml_map: ElementTree) -> (Tensor, Tensor):
     root = xml_map.getroot()
-    map_width = int(root.attrib['width'])
-    map_height = int(root.attrib['height'])
+    map_width = int(root.attrib["width"])
+    map_height = int(root.attrib["height"])
     if map_width != map_height:
         raise Exception("Width and height dimensions do not match!")
 
     # Extract Terrain Features
-    terrain_data = root.find('terrain').text.strip()
-    terrain_values = [int(value) for value in terrain_data]  # Convert each character to an integer
-    terrain_tensor = torch.tensor(terrain_values, dtype=torch.float32).reshape(map_width, map_height)
+    terrain_data = root.find("terrain").text.strip()
+    terrain_values = [
+        int(value) for value in terrain_data
+    ]  # Convert each character to an integer
+    terrain_tensor = torch.tensor(terrain_values, dtype=torch.float32).reshape(
+        map_width, map_height
+    )
     empty_space_tensor = (terrain_tensor == 0).type(torch.float32)
     wall_tensor = (terrain_tensor == 1).type(torch.float32)
 
     # Process Units
-    units = root.find('units')
+    units = root.find("units")
     resources = []
     other_units = []
     for unit in units:
@@ -39,13 +44,35 @@ def convert_xml(xml_map: ElementTree) -> (Tensor, Tensor):
     ten_resource_tensor = torch.zeros_like(wall_tensor, dtype=torch.float32)
     fifteen_resource_tensor = torch.zeros_like(wall_tensor, dtype=torch.float32)
     twenty_resource_tensor = torch.zeros_like(wall_tensor, dtype=torch.float32)
-    map_tensor = torch.stack([empty_space_tensor, wall_tensor, five_resource_tensor, ten_resource_tensor, fifteen_resource_tensor, twenty_resource_tensor], dim=0)
+    map_tensor = torch.stack(
+        [
+            empty_space_tensor,
+            wall_tensor,
+            five_resource_tensor,
+            ten_resource_tensor,
+            fifteen_resource_tensor,
+            twenty_resource_tensor,
+        ],
+        dim=0,
+    )
 
     for resource in resources:
         x_cord = int(resource.attrib["x"])
         y_cord = int(resource.attrib["x"])
         resource_amount = int(resource.attrib["resources"])
-        layer = LayerName.FIVE_RESOURCES if resource_amount == 5 else LayerName.TEN_RESOURCES if resource_amount == 10 else LayerName.FIFTEEN_RESOURCES if resource_amount == 15 else LayerName.TWENTY_RESOURCES
+        layer = (
+            LayerName.FIVE_RESOURCES
+            if resource_amount == 5
+            else (
+                LayerName.TEN_RESOURCES
+                if resource_amount == 10
+                else (
+                    LayerName.FIFTEEN_RESOURCES
+                    if resource_amount == 15
+                    else LayerName.TWENTY_RESOURCES
+                )
+            )
+        )
         map_tensor[layer.value, x_cord, y_cord] = 1.0
 
     """
@@ -56,31 +83,48 @@ def convert_xml(xml_map: ElementTree) -> (Tensor, Tensor):
     for other in other_units:
         x_cord = int(other.attrib["x"])
         y_cord = int(other.attrib["x"])
-        invalid_actions_map[ x_cord, y_cord] = 1.0
+        invalid_actions_map[x_cord, y_cord] = 1.0
 
     return (map_tensor, invalid_actions_map)
 
-def update_xml_map(file_path, xml_map, layer_update: LayerName, layer_old: LayerName, x_cord, y_cord):
+
+def update_xml_map(
+    file_path, xml_map, layer_update: LayerName, layer_old: LayerName, x_cord, y_cord
+):
     root = xml_map.getroot()
-    map_height = int(root.attrib['height'])
+    map_height = int(root.attrib["height"])
 
     # Update terrain text
-    terrain_node = root.find('terrain')
+    terrain_node = root.find("terrain")
     terrain_text = terrain_node.text
     update_index = y_cord * map_height + x_cord
-    updated_terrain = terrain_text[:update_index] +( "1" if layer_update.value == 1 else "0") + terrain_text[update_index + 1:]
-    terrain_node.text = updated_terrain\
-
+    updated_terrain = (
+        terrain_text[:update_index]
+        + ("1" if layer_update.value == 1 else "0")
+        + terrain_text[update_index + 1 :]
+    )
+    terrain_node.text = updated_terrain
     # Unit updates
-    units = root.find('units')
-    resource_amount = 5 if layer_update == LayerName.FIVE_RESOURCES else 10 if layer_update == LayerName.TEN_RESOURCES else 15 if layer_update == LayerName.FIFTEEN_RESOURCES else 20
+    units = root.find("units")
+    resource_amount = (
+        5
+        if layer_update == LayerName.FIVE_RESOURCES
+        else (
+            10
+            if layer_update == LayerName.TEN_RESOURCES
+            else 15 if layer_update == LayerName.FIFTEEN_RESOURCES else 20
+        )
+    )
     resource_amount = str(resource_amount)
 
     # Change Existing Resource Amount
     if layer_old.value > 1 and layer_update.value > 1:
         for unit in units:
-            if int(unit.attrib.get('x')) == x_cord and int(unit.attrib.get('y')) == y_cord:
-                unit.set('resources', resource_amount)
+            if (
+                int(unit.attrib.get("x")) == x_cord
+                and int(unit.attrib.get("y")) == y_cord
+            ):
+                unit.set("resources", resource_amount)
                 break
     elif layer_old.value < 2 and layer_update.value > 1:
         # We need to add a new sub-element to the XML
@@ -96,7 +140,10 @@ def update_xml_map(file_path, xml_map, layer_update: LayerName, layer_old: Layer
         )
     elif layer_old.value > 1 and layer_update.value < 2:
         for unit in units:
-            if int(unit.attrib.get('x')) == x_cord and int(unit.attrib.get('y')) == y_cord:
+            if (
+                int(unit.attrib.get("x")) == x_cord
+                and int(unit.attrib.get("y")) == y_cord
+            ):
                 units.remove(unit)
                 break
 
@@ -109,8 +156,12 @@ if __name__ == "__main__":
     convert_xml(xml_map)
 
     # Change existing resource amount
-    update_xml_map(file_path, xml_map, LayerName.TWENTY_RESOURCES, LayerName.FIVE_RESOURCES, 15, 15)
+    update_xml_map(
+        file_path, xml_map, LayerName.TWENTY_RESOURCES, LayerName.FIVE_RESOURCES, 15, 15
+    )
     # Remove old tag
     update_xml_map(file_path, xml_map, LayerName.WALL, LayerName.TEN_RESOURCES, 0, 0)
     # Add new tag
-    update_xml_map(file_path, xml_map, LayerName.FIFTEEN_RESOURCES, LayerName.EMPTY, 8, 4)
+    update_xml_map(
+        file_path, xml_map, LayerName.FIFTEEN_RESOURCES, LayerName.EMPTY, 8, 4
+    )
