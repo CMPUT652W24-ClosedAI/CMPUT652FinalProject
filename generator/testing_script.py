fa37b5714512430886829d8795ce3068f1bc32f2
Seth Akins <sethlinc4@gmail.com>
2024-11-18 19:29:09 -0700

    Used longer trained model to produce map

--- generator/testing_script.py
+++ generator/testing_script.py
@@ -8,22 +8,20 @@ from generator.map_utils import update_xml_map, convert_xml
 from generator.unet_generator import Unet
 
 
-def generate_map(map_path: str):
+def generate_map(input_map_path: str, output_map_path: str, model_path: str):
     policy_net = Unet()
-    policy_net.load_state_dict(torch.load("policy_net.pt"))
+    policy_net.load_state_dict(torch.load(model_path))
     target_net = Unet()
     target_net.load_state_dict(policy_net.state_dict())
 
-    # Test Using convert xml
-    shutil.copy("defaultMap.xml", "outputMap.xml")
-    file_path = "outputMap.xml"
-    xml_map = ET.parse(file_path)
+    shutil.copy(input_map_path, output_map_path)
+    xml_map = ET.parse(output_map_path)
     tensor_map, invalid_actions_mask = convert_xml(xml_map)
 
     state = tensor_map
     id = 100
 
-    for step in range(100):
+    for step in range(64):
         with torch.no_grad():
             q_values = policy_net(state)
             # mask q_values
@@ -41,9 +39,9 @@ def generate_map(map_path: str):
             else:
                 old_index = test_index.item()
 
-            update_xml_map("outputMap.xml", LayerName(action[0].item()), LayerName(old_index), action[1].item(), action[2].item(), id)
+            update_xml_map(output_map_path, LayerName(action[0].item()), LayerName(old_index), action[1].item(), action[2].item(), id)
             id += 1
 
 
 if __name__ == '__main__':
-    generate_map("defaultMap.xml")
+    generate_map("defaultMap.xml", "outputMap_larger.xml", "policy_net_longer_training.pt")
